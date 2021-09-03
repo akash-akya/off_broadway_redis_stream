3e2294939486aa9788cb32dcafc885c1d915d647
akash-akya <akashh246@gmail.com>
2021-09-03 20:10:30 +0530

    Bump to v0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OffBroadwayRedisStream.MixProject do
   def project do
     [
       app: :off_broadway_redis_stream,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
