c3838dbee01ddfe6d788a7af7335b8a6e7e1d158
akash-akya <akashh246@gmail.com>
2023-08-25 12:10:34 +0530

    Use normal stop instead of kill in tests

--- test/support/test_helper.ex
+++ test/support/test_helper.ex
@@ -1,22 +1,11 @@
 defmodule TestHelper do
-  def stop_process(pid) do
-    Process.flag(:trap_exit, true)
-
-    ref = Process.monitor(pid)
-    Process.exit(pid, :kill)
-
-    receive do
-      {:DOWN, ^ref, _, _, _} -> :ok
-    end
-  end
-
   def redix_opts, do: [host: host(), port: port()]
 
-  defp host do
+  def host do
     System.get_env("REDIS_HOST") || "localhost"
   end
 
-  defp port do
+  def port do
     if p = System.get_env("REDIS_PORT") do
       {port, ""} = Integer.parse(p)
       port
@@ -24,5 +13,4 @@ defmodule TestHelper do
       6379
     end
   end
-
 end
